sanchitachaurasia/DBIS-Project

commit 5aa24336059f97c1c1093ef16c4199e50261e401

Improve some ancient, crufty code in bootstrap + initdb.
At some point back in the last century, somebody felt that reading
all of pg_type twice was cheaper, or at least easier, than using
repalloc() to resize the Typ[] array dynamically.  That seems like an
entirely wacko proposition, so rewrite the code to do it the other
way.  (To add insult to injury, there were two not-quite-identical
copies of said code.)

initdb.c's readfile() function had the same disease of preferring
to do double the I/O to avoid resizing its output array.  Here,
we can make things easier by using the just-invented pg_get_line()
function to handle reading individual lines without a predetermined
notion of how long they are.

On my machine, it's difficult to detect any net change in the
overall runtime of initdb from these changes; but they should
help on slower buildfarm machines (especially since a buildfarm
cycle involves a lot of initdb's these days).

My attention was drawn to these places by scan-build complaints,
but on inspection they needed a lot more work than just suppressing
dead stores :-(

src/bin/initdb/initdb.c
@@ -468,14 +468,11 @@ filter_lines_with_token(char **lines, const char *token)
 static char **
 readfile(const char *path)
 {
-	FILE	   *infile;
-	int			maxlength = 1,
-				linelen = 0;
-	int			nlines = 0;
-	int			n;
 	char	  **result;
-	char	   *buffer;
-	int			c;
+	FILE	   *infile;
+	int			maxlines;
+	int			n;
+	char	   *ln;
 
 	if ((infile = fopen(path, "r")) == NULL)
 	{
@@ -483,39 +480,24 @@ readfile(const char *path)
 		exit(1);
 	}
 
-	/* pass over the file twice - the first time to size the result */
+	maxlines = 1024;
+	result = (char **) pg_malloc(maxlines * sizeof(char *));
 
-	while ((c = fgetc(infile)) != EOF)
-	{
-		linelen++;
-		if (c == '\n')
-		{
-			nlines++;
-			if (linelen > maxlength)
-				maxlength = linelen;
-			linelen = 0;
-		}
-	}
-
-	/* handle last line without a terminating newline (yuck) */
-	if (linelen)
-		nlines++;
-	if (linelen > maxlength)
-		maxlength = linelen;
-
-	/* set up the result and the line buffer */
-	result = (char **) pg_malloc((nlines + 1) * sizeof(char *));
-	buffer = (char *) pg_malloc(maxlength + 1);
-
-	/* now reprocess the file and store the lines */
-	rewind(infile);
 	n = 0;
-	while (fgets(buffer, maxlength + 1, infile) != NULL && n < nlines)
-		result[n++] = pg_strdup(buffer);
+	while ((ln = pg_get_line(infile)) != NULL)
+	{
+		/* make sure there will be room for a trailing NULL pointer */
+		if (n >= maxlines - 1)
+		{
+			maxlines *= 2;
+			result = (char **) pg_realloc(result, maxlines * sizeof(char *));
+		}
+
+		result[n++] = ln;
+	}
+	result[n] = NULL;
 
 	fclose(infile);
-	free(buffer);
-	result[n] = NULL;
 
 	return result;
 }

src/backend/bootstrap/bootstrap.c
@@ -53,14 +53,12 @@
 uint32		bootstrap_data_checksum_version = 0;	/* No checksum */
 
 
-#define ALLOC(t, c) \
-	((t *) MemoryContextAllocZero(TopMemoryContext, (unsigned)(c) * sizeof(t)))
-
 static void CheckerModeMain(void);
 static void BootstrapModeMain(void);
 static void bootstrap_signals(void);
 static void ShutdownAuxiliaryProcess(int code, Datum arg);
 static Form_pg_attribute AllocateAttribute(void);
+static void populate_typ_array(void);
 static Oid	gettype(char *type);
 static void cleanup(void);
 
@@ -583,46 +581,24 @@ ShutdownAuxiliaryProcess(int code, Datum arg)
 
 /* ----------------
  *		boot_openrel
+ *
+ * Execute BKI OPEN command.
  * ----------------
  */
 void
 boot_openrel(char *relname)
 {
 	int			i;
-	struct typmap **app;
-	Relation	rel;
-	TableScanDesc scan;
-	HeapTuple	tup;
 
 	if (strlen(relname) >= NAMEDATALEN)
 		relname[NAMEDATALEN - 1] = '\0';
 
+	/*
+	 * pg_type must be filled before any OPEN command is executed, hence we
+	 * can now populate the Typ array if we haven't yet.
+	 */
 	if (Typ == NULL)
-	{
-		/* We can now load the pg_type data */
-		rel = table_open(TypeRelationId, NoLock);
-		scan = table_beginscan_catalog(rel, 0, NULL);
-		i = 0;
-		while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
-			++i;
-		table_endscan(scan);
-		app = Typ = ALLOC(struct typmap *, i + 1);
-		while (i-- > 0)
-			*app++ = ALLOC(struct typmap, 1);
-		*app = NULL;
-		scan = table_beginscan_catalog(rel, 0, NULL);
-		app = Typ;
-		while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
-		{
-			(*app)->am_oid = ((Form_pg_type) GETSTRUCT(tup))->oid;
-			memcpy((char *) &(*app)->am_typ,
-				   (char *) GETSTRUCT(tup),
-				   sizeof((*app)->am_typ));
-			app++;
-		}
-		table_endscan(scan);
-		table_close(rel, NoLock);
-	}
+		populate_typ_array();
 
 	if (boot_reldesc != NULL)
 		closerel(NULL);
@@ -890,6 +866,52 @@ cleanup(void)
 }
 
 /* ----------------
+ *		populate_typ_array
+ *
+ * Load the Typ array by reading pg_type.
+ * ----------------
+ */
+static void
+populate_typ_array(void)
+{
+	Relation	rel;
+	TableScanDesc scan;
+	HeapTuple	tup;
+	int			nalloc;
+	int			i;
+
+	Assert(Typ == NULL);
+
+	nalloc = 512;
+	Typ = (struct typmap **)
+		MemoryContextAlloc(TopMemoryContext, nalloc * sizeof(struct typmap *));
+
+	rel = table_open(TypeRelationId, NoLock);
+	scan = table_beginscan_catalog(rel, 0, NULL);
+	i = 0;
+	while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
+	{
+		Form_pg_type typForm = (Form_pg_type) GETSTRUCT(tup);
+
+		/* make sure there will be room for a trailing NULL pointer */
+		if (i >= nalloc - 1)
+		{
+			nalloc *= 2;
+			Typ = (struct typmap **)
+				repalloc(Typ, nalloc * sizeof(struct typmap *));
+		}
+		Typ[i] = (struct typmap *)
+			MemoryContextAlloc(TopMemoryContext, sizeof(struct typmap));
+		Typ[i]->am_oid = typForm->oid;
+		memcpy(&(Typ[i]->am_typ), typForm, sizeof(Typ[i]->am_typ));
+		i++;
+	}
+	Typ[i] = NULL;				/* Fill trailing NULL pointer */
+	table_endscan(scan);
+	table_close(rel, NoLock);
+}
+
+/* ----------------
  *		gettype
  *
  * NB: this is really ugly; it will return an integer index into TypInfo[],
@@ -903,14 +925,10 @@ cleanup(void)
 static Oid
 gettype(char *type)
 {
-	int			i;
-	Relation	rel;
-	TableScanDesc scan;
-	HeapTuple	tup;
-	struct typmap **app;
-
 	if (Typ != NULL)
 	{
+		struct typmap **app;
+
 		for (app = Typ; *app != NULL; app++)
 		{
 			if (strncmp(NameStr((*app)->am_typ.typname), type, NAMEDATALEN) == 0)
@@ -922,33 +940,16 @@ gettype(char *type)
 	}
 	else
 	{
+		int			i;
+
 		for (i = 0; i < n_types; i++)
 		{
 			if (strncmp(type, TypInfo[i].name, NAMEDATALEN) == 0)
 				return i;
 		}
+		/* Not in TypInfo, so we'd better be able to read pg_type now */
 		elog(DEBUG4, "external type: %s", type);
-		rel = table_open(TypeRelationId, NoLock);
-		scan = table_beginscan_catalog(rel, 0, NULL);
-		i = 0;
-		while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
-			++i;
-		table_endscan(scan);
-		app = Typ = ALLOC(struct typmap *, i + 1);
-		while (i-- > 0)
-			*app++ = ALLOC(struct typmap, 1);
-		*app = NULL;
-		scan = table_beginscan_catalog(rel, 0, NULL);
-		app = Typ;
-		while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
-		{
-			(*app)->am_oid = ((Form_pg_type) GETSTRUCT(tup))->oid;
-			memmove((char *) &(*app++)->am_typ,
-					(char *) GETSTRUCT(tup),
-					sizeof((*app)->am_typ));
-		}
-		table_endscan(scan);
-		table_close(rel, NoLock);
+		populate_typ_array();
 		return gettype(type);
 	}
 	elog(ERROR, "unrecognized type \"%s\"", type);

commit 68baa98f45c10b9f7fdf46937b39dc278a832fc3

Refactor pg_get_line() to expose an alternative StringInfo-based API.
Letting the caller provide a StringInfo to read into is helpful when
the caller needs to merge lines or otherwise modify the data after
it's been read.  Notably, now the code added by commit c53898e35
can use pg_get_line_append() instead of having its own copy of that
logic.  A follow-on commit will also make use of this.

Also, since StringInfo buffers are a minimum of 1KB long, blindly
using pg_get_line() in a loop can eat a lot more memory than one would
expect.  I discovered for instance that commit 5aa243360 caused initdb
to consume circa 10MB to read postgres.bki, even though that's under
1MB worth of data.  A less memory-hungry alternative is to re-use the
same StringInfo for all lines and pg_strdup the results.

Discussion: https://postgr.es/m/1315832.1599345736@sss.pgh.pa.us

src/backend/libpq/hba.c
@@ -502,33 +502,8 @@ tokenize_file(const char *filename, FILE *file, List **tok_lines, int elevel)
 		/* Collect the next input line, handling backslash continuations */
 		resetStringInfo(&buf);
 
-		while (!feof(file) && !ferror(file))
+		while (pg_get_line_append(file, &buf))
 		{
-			/* Make sure there's a reasonable amount of room in the buffer */
-			enlargeStringInfo(&buf, 128);
-
-			/* Read some data, appending it to what we already have */
-			if (fgets(buf.data + buf.len, buf.maxlen - buf.len, file) == NULL)
-			{
-				int			save_errno = errno;
-
-				if (!ferror(file))
-					break;		/* normal EOF */
-				/* I/O error! */
-				ereport(elevel,
-						(errcode_for_file_access(),
-						 errmsg("could not read file \"%s\": %m", filename)));
-				err_msg = psprintf("could not read file \"%s\": %s",
-								   filename, strerror(save_errno));
-				resetStringInfo(&buf);
-				break;
-			}
-			buf.len += strlen(buf.data + buf.len);
-
-			/* If we haven't got a whole line, loop to read more */
-			if (!(buf.len > 0 && buf.data[buf.len - 1] == '\n'))
-				continue;
-
 			/* Strip trailing newline, including \r in case we're on Windows */
 			buf.len = pg_strip_crlf(buf.data);
 
@@ -548,6 +523,19 @@ tokenize_file(const char *filename, FILE *file, List **tok_lines, int elevel)
 			}
 
 			/* Nope, so we have the whole line */
+			break;
+		}
+
+		if (ferror(file))
+		{
+			/* I/O error! */
+			int			save_errno = errno;
+
+			ereport(elevel,
+					(errcode_for_file_access(),
+					 errmsg("could not read file \"%s\": %m", filename)));
+			err_msg = psprintf("could not read file \"%s\": %s",
+							   filename, strerror(save_errno));
 			break;
 		}
 

src/bin/initdb/initdb.c
@@ -470,9 +470,9 @@ readfile(const char *path)
 {
 	char	  **result;
 	FILE	   *infile;
+	StringInfoData line;
 	int			maxlines;
 	int			n;
-	char	   *ln;
 
 	if ((infile = fopen(path, "r")) == NULL)
 	{
@@ -480,11 +480,13 @@ readfile(const char *path)
 		exit(1);
 	}
 
+	initStringInfo(&line);
+
 	maxlines = 1024;
 	result = (char **) pg_malloc(maxlines * sizeof(char *));
 
 	n = 0;
-	while ((ln = pg_get_line(infile)) != NULL)
+	while (pg_get_line_append(infile, &line))
 	{
 		/* make sure there will be room for a trailing NULL pointer */
 		if (n >= maxlines - 1)
@@ -493,9 +495,13 @@ readfile(const char *path)
 			result = (char **) pg_realloc(result, maxlines * sizeof(char *));
 		}
 
-		result[n++] = ln;
+		result[n++] = pg_strdup(line.data);
+
+		resetStringInfo(&line);
 	}
 	result[n] = NULL;
+
+	pfree(line.data);
 
 	fclose(infile);
 

src/common/pg_get_line.c
@@ -41,6 +41,11 @@
  * Note that while I/O errors are reflected back to the caller to be
  * dealt with, an OOM condition for the palloc'd buffer will not be;
  * there'll be an ereport(ERROR) or exit(1) inside stringinfo.c.
+ *
+ * Also note that the palloc'd buffer is usually a lot longer than
+ * strictly necessary, so it may be inadvisable to use this function
+ * to collect lots of long-lived data.  A less memory-hungry option
+ * is to use pg_get_line_append() in a loop, then pstrdup() each line.
  */
 char *
 pg_get_line(FILE *stream)
@@ -49,21 +54,7 @@ pg_get_line(FILE *stream)
 
 	initStringInfo(&buf);
 
-	/* Read some data, appending it to whatever we already have */
-	while (fgets(buf.data + buf.len, buf.maxlen - buf.len, stream) != NULL)
-	{
-		buf.len += strlen(buf.data + buf.len);
-
-		/* Done if we have collected a newline */
-		if (buf.len > 0 && buf.data[buf.len - 1] == '\n')
-			return buf.data;
-
-		/* Make some more room in the buffer, and loop to read more data */
-		enlargeStringInfo(&buf, 128);
-	}
-
-	/* Did fgets() fail because of an I/O error? */
-	if (ferror(stream))
+	if (!pg_get_line_append(stream, &buf))
 	{
 		/* ensure that free() doesn't mess up errno */
 		int			save_errno = errno;
@@ -73,13 +64,50 @@ pg_get_line(FILE *stream)
 		return NULL;
 	}
 
-	/* If we read no data before reaching EOF, we should return NULL */
-	if (buf.len == 0)
+	return buf.data;
+}
+
+/*
+ * pg_get_line_append()
+ *
+ * This has similar behavior to pg_get_line(), and thence to fgets(),
+ * except that the collected data is appended to whatever is in *buf.
+ *
+ * Returns true if a line was successfully collected (including the
+ * case of a non-newline-terminated line at EOF).  Returns false if
+ * there was an I/O error or no data was available before EOF.
+ * (Check ferror(stream) to distinguish these cases.)
+ *
+ * In the false-result case, the contents of *buf are logically unmodified,
+ * though it's possible that the buffer has been resized.
+ */
+bool
+pg_get_line_append(FILE *stream, StringInfo buf)
+{
+	int			orig_len = buf->len;
+
+	/* Read some data, appending it to whatever we already have */
+	while (fgets(buf->data + buf->len, buf->maxlen - buf->len, stream) != NULL)
 	{
-		pfree(buf.data);
-		return NULL;
+		buf->len += strlen(buf->data + buf->len);
+
+		/* Done if we have collected a newline */
+		if (buf->len > orig_len && buf->data[buf->len - 1] == '\n')
+			return true;
+
+		/* Make some more room in the buffer, and loop to read more data */
+		enlargeStringInfo(buf, 128);
 	}
 
-	/* No newline at EOF ... so return what we have */
-	return buf.data;
+	/* Check for I/O errors and EOF */
+	if (ferror(stream) || buf->len == orig_len)
+	{
+		/* Discard any data we collected before detecting error */
+		buf->len = orig_len;
+		buf->data[orig_len] = '\0';
+		return false;
+	}
+
+	/* No newline at EOF, but we did collect some data */
+	return true;
 }

src/include/common/string.h
@@ -10,6 +10,8 @@
 #ifndef COMMON_STRING_H
 #define COMMON_STRING_H
 
+struct StringInfoData;			/* avoid including stringinfo.h here */
+
 /* functions in src/common/string.c */
 extern bool pg_str_endswith(const char *str, const char *end);
 extern int	strtoint(const char *pg_restrict str, char **pg_restrict endptr,
@@ -19,6 +21,7 @@ extern int	pg_strip_crlf(char *str);
 
 /* functions in src/common/pg_get_line.c */
 extern char *pg_get_line(FILE *stream);
+extern bool pg_get_line_append(FILE *stream, struct StringInfoData *buf);
 
 /* functions in src/common/sprompt.c */
 extern char *simple_prompt(const char *prompt, bool echo);