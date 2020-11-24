sanchitachaurasia/DBIS-Project

commit 8efc1220c1efe3cf9e26bd826d2fc3d6cfff7399

Define pg_attribute_cold and pg_attribute_hot macros
For compilers supporting __has_attribute and __has_attribute (hot/cold).

__has_attribute is supported on gcc >= 5, clang >= 2.9 and icc >= 17.

A followup commit will implement some usages of these macros.

Author: David Rowley
Reviewed-by: Andres Freund, Peter Eisentraut
Discussion: https://postgr.es/m/CAApHDvrVpasrEzLL2er7p9iwZFZ%3DJj6WisePcFeunwfrV0js_A%40mail.gmail.com

src/include/c.h
@@ -192,6 +192,22 @@
 #endif
 
 /*
+ * Marking certain functions as "hot" or "cold" can be useful to assist the
+ * compiler in arranging the assembly code in a more efficient way.
+ */
+#if defined(__has_attribute) && __has_attribute (cold)
+#define pg_attribute_cold __attribute__((cold))
+#else
+#define pg_attribute_cold
+#endif
+
+#if defined(__has_attribute) && __has_attribute (hot)
+#define pg_attribute_hot __attribute__((hot))
+#else
+#define pg_attribute_hot
+#endif
+
+/*
  * Mark a point as unreachable in a portable fashion.  This should preferably
  * be something that the compiler understands, to aid code generation.
  * In assert-enabled builds, we prefer abort() for debugging reasons.

commit 3a933c746e6db72cc7199f225aaf99bf4be7bfe5

Tidy up definitions of pg_attribute_hot and pg_attribute_cold
299f543c1 was a quick fix for portability problem I introduced in
8efc1220c.  299f543c1 adds a few more cases to the preprocessor logic than
I'd have liked.  Andres Freund and Dagfinn Ilmari Mannsåker suggested a
better way to do this.

In passing, also adjust the only current usage of these macros so that the
macro comes before the function's return type in the declaration of the
function.  This now matches what the definition of the function does.

Discussion: https://postgr.es/m/20200625163553.lt6wocbjhklp5pl4@alap3.anarazel.de
Discussion: https://postgr.es/m/87pn43bmok.fsf@wibble.ilmari.org

src/include/c.h
@@ -104,6 +104,15 @@
  * XLC: https://www.ibm.com/support/knowledgecenter/SSGH2K_13.1.2/com.ibm.xlc131.aix.doc/language_ref/type_attrib.html
  */
 
+/*
+ * For compilers which don't support __has_attribute, we just define
+ * __has_attribute(x) to 0 so that we can define macros for various
+ * __attribute__s more easily below.
+ */
+#ifndef __has_attribute
+#define __has_attribute(attribute) 0
+#endif
+
 /* only GCC supports the unused attribute */
 #ifdef __GNUC__
 #define pg_attribute_unused() __attribute__((unused))
@@ -195,8 +204,6 @@
  * Marking certain functions as "hot" or "cold" can be useful to assist the
  * compiler in arranging the assembly code in a more efficient way.
  */
-#if defined(__has_attribute)
-
 #if __has_attribute (cold)
 #define pg_attribute_cold __attribute__((cold))
 #else
@@ -207,11 +214,6 @@
 #define pg_attribute_hot __attribute__((hot))
 #else
 #define pg_attribute_hot
-#endif
-
-#else
-#define pg_attribute_hot
-#define pg_attribute_cold
 #endif
 
 /*

src/include/utils/elog.h
@@ -160,7 +160,7 @@
 extern bool message_level_is_interesting(int elevel);
 
 extern bool errstart(int elevel, const char *domain);
-extern bool pg_attribute_cold errstart_cold(int elevel, const char *domain);
+extern pg_attribute_cold bool errstart_cold(int elevel, const char *domain);
 extern void errfinish(const char *filename, int lineno, const char *funcname);
 
 extern int	errcode(int sqlerrcode);