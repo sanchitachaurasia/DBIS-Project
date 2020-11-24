sanchitachaurasia/DBIS-Project

commit 8efc1220c1efe3cf9e26bd826d2fc3d6cfff7399

Define pg_attribute_cold and pg_attribute_hot macros
For compilers supporting __has_attribute and __has_attribute (hot/cold).

__has_attribute is supported on gcc >= 5, clang >= 2.9 and icc >= 17.

A followup commit will implement some usages of these macros.

Author: David Rowley
Reviewed-by: Andres Freund, Peter Eisentraut
Discussion: https://postgr.es/m/CAApHDvrVpasrEzLL2er7p9iwZFZ%3DJj6WisePcFeunwfrV0js_A%40mail.gmail.com

src/include/c.h
@@ -192,6 +192,22 @@
 #endif
 
 /*
+ * Marking certain functions as "hot" or "cold" can be useful to assist the
+ * compiler in arranging the assembly code in a more efficient way.
+ */
+#if defined(__has_attribute) && __has_attribute (cold)
+#define pg_attribute_cold __attribute__((cold))
+#else
+#define pg_attribute_cold
+#endif
+
+#if defined(__has_attribute) && __has_attribute (hot)
+#define pg_attribute_hot __attribute__((hot))
+#else
+#define pg_attribute_hot
+#endif
+
+/*
  * Mark a point as unreachable in a portable fashion.  This should preferably
  * be something that the compiler understands, to aid code generation.
  * In assert-enabled builds, we prefer abort() for debugging reasons.

commit 299f543c120dabd53c55000f5492bba2e43cc825

Fix unportable usage of __has_attribute
This should fix the breakages caused by 8efc1220c, which seems to break
the build for GCC version < 5.

It seems, according to the GCC manual that __has_attribute is a "special
operator" and must be tested without any other conditions in the
preprocessor test.

Per recommendation from the GCC manual via Greg Nancarrow

Reported-by: Greg Nancarrow
Discussion: https://postgr.es/m/CAJcOf-euSu8fhC10v476o9dqnjqKysVs1_vRms-_fvajpZ3kFw@mail.gmail.com

src/include/c.h
@@ -195,16 +195,23 @@
  * Marking certain functions as "hot" or "cold" can be useful to assist the
  * compiler in arranging the assembly code in a more efficient way.
  */
-#if defined(__has_attribute) && __has_attribute (cold)
+#if defined(__has_attribute)
+
+#if __has_attribute (cold)
 #define pg_attribute_cold __attribute__((cold))
 #else
 #define pg_attribute_cold
 #endif
 
-#if defined(__has_attribute) && __has_attribute (hot)
+#if __has_attribute (hot)
 #define pg_attribute_hot __attribute__((hot))
 #else
 #define pg_attribute_hot
+#endif
+
+#else
+#define pg_attribute_hot
+#define pg_attribute_cold
 #endif
 
 /*