sanchitachaurasia/DBIS-Project

commit 5cf3de23aaaf76dcc51b87b1691f4f07a9d2a6c2

Allow configurable LZ4 TOAST compression.
There is now a per-column COMPRESSION option which can be set to pglz
(the default, and the only option in up until now) or lz4. Or, if you
like, you can set the new default_toast_compression GUC to lz4, and
then that will be the default for new table columns for which no value
is specified. We don't have lz4 support in the PostgreSQL code, so
to use lz4 compression, PostgreSQL must be built --with-lz4.

In general, TOAST compression means compression of individual column
values, not the whole tuple, and those values can either be compressed
inline within the tuple or compressed and then stored externally in
the TOAST table, so those properties also apply to this feature.

Prior to this commit, a TOAST pointer has two unused bits as part of
the va_extsize field, and a compessed datum has two unused bits as
part of the va_rawsize field. These bits are unused because the length
of a varlena is limited to 1GB; we now use them to indicate the
compression type that was used. This means we only have bit space for
2 more built-in compresison types, but we could work around that
problem, if necessary, by introducing a new vartag_external value for
any further types we end up wanting to add. Hopefully, it won't be
too important to offer a wide selection of algorithms here, since
each one we add not only takes more coding but also adds a build
dependency for every packager. Nevertheless, it seems worth doing
at least this much, because LZ4 gets better compression than PGLZ
with less CPU usage.

It's possible for LZ4-compressed datums to leak into composite type
values stored on disk, just as it is for PGLZ. It's also possible for
LZ4-compressed attributes to be copied into a different table via SQL
commands such as CREATE TABLE AS or INSERT .. SELECT.  It would be
expensive to force such values to be decompressed, so PostgreSQL has
never done so. For the same reasons, we also don't force recompression

src/backend/access/heap/heapam_handler.c
@@ -19,6 +19,7 @@
  */
 #include "postgres.h"
 
+#include "access/detoast.h"
 #include "access/genam.h"
 #include "access/heapam.h"
 #include "access/heaptoast.h"
@@ -26,6 +27,7 @@
 #include "access/rewriteheap.h"
 #include "access/syncscan.h"
 #include "access/tableam.h"
+#include "access/toast_compression.h"
 #include "access/tsmapi.h"
 #include "access/xact.h"
 #include "catalog/catalog.h"
@@ -2469,6 +2471,44 @@ reform_and_rewrite_tuple(HeapTuple tuple,
 	{
 		if (TupleDescAttr(newTupDesc, i)->attisdropped)
 			isnull[i] = true;
+
+		/*
+		 * Use this opportunity to force recompression of any data that's
+		 * compressed with some TOAST compression method other than the one
+		 * configured for the column.  We don't actually need to perform the
+		 * compression here; we just need to decompress. That will trigger
+		 * recompression later on.
+		 */
+		else if (!isnull[i] && TupleDescAttr(newTupDesc, i)->attlen == -1)
+		{
+			struct varlena *new_value;
+			ToastCompressionId	cmid;
+			char	cmethod;
+
+			new_value = (struct varlena *) DatumGetPointer(values[i]);
+			cmid = toast_get_compression_id(new_value);
+
+			/* nothing to be done for uncompressed data */
+			if (cmid == TOAST_INVALID_COMPRESSION_ID)
+				continue;
+
+			/* convert compression id to compression method */
+			switch (cmid)
+			{
+				case TOAST_PGLZ_COMPRESSION_ID:
+					cmethod = TOAST_PGLZ_COMPRESSION;
+					break;
+				case TOAST_LZ4_COMPRESSION_ID:
+					cmethod = TOAST_LZ4_COMPRESSION;
+					break;
+				default:
+					elog(ERROR, "invalid compression method id %d", cmid);
+			}
+
+			/* if compression method doesn't match then detoast the value */
+			if (TupleDescAttr(newTupDesc, i)->attcompression != cmethod)
+				values[i] = PointerGetDatum(detoast_attr(new_value));
+		}
 	}
 
 	copiedTuple = heap_form_tuple(newTupDesc, values, isnull);

commit 3975fdb1918fe0375517b633e73e96e333d6c5e4

Fix memory leak when de-toasting compressed values in VACUUM FULL/CLUSTER
VACUUM FULL and CLUSTER can be used to enforce the use of the existing
compression method of a toastable column if a value currently stored is
compressed with a method that does not match the column's defined
method.  The code in charge of decompressing and recompressing toast
values at rewrite left around the detoasted values, causing an
accumulation of memory allocated in TopTransactionContext.

When processing large relations, this could cause the system to run out
of memory.  The detoasted values are not needed once their tuple is
rewritten, and this commit ensures that the necessary cleanup happens.

Issue introduced by 5cf3de23.  The comments of the area are reordered a
bit while on it.

Reported-by: Andres Freund
Analyzed-by: Andres Freund
Author: Michael Paquier
Reviewed-by: Dilip Kumar
Discussion: https://postgr.es/m/20210521211929.pcehg6f23icwstdb@alap3.anarazel.de

src/backend/access/heap/heapam_handler.c
@@ -2463,24 +2463,27 @@ reform_and_rewrite_tuple(HeapTuple tuple,
 	TupleDesc	newTupDesc = RelationGetDescr(NewHeap);
 	HeapTuple	copiedTuple;
 	int			i;
+	bool		values_free[MaxTupleAttributeNumber];
+
+	memset(values_free, 0, newTupDesc->natts * sizeof(bool));
 
 	heap_deform_tuple(tuple, oldTupDesc, values, isnull);
 
-	/* Be sure to null out any dropped columns */
 	for (i = 0; i < newTupDesc->natts; i++)
 	{
+		/* Be sure to null out any dropped columns */
 		if (TupleDescAttr(newTupDesc, i)->attisdropped)
 			isnull[i] = true;
-
-		/*
-		 * Use this opportunity to force recompression of any data that's
-		 * compressed with some TOAST compression method other than the one
-		 * configured for the column.  We don't actually need to perform the
-		 * compression here; we just need to decompress. That will trigger
-		 * recompression later on.
-		 */
 		else if (!isnull[i] && TupleDescAttr(newTupDesc, i)->attlen == -1)
 		{
+			/*
+			 * Use this opportunity to force recompression of any data that's
+			 * compressed with some TOAST compression method other than the
+			 * one configured for the column.  We don't actually need to
+			 * perform the compression here; we just need to decompress.  That
+			 * will trigger recompression later on.
+			 */
+
 			struct varlena *new_value;
 			ToastCompressionId cmid;
 			char		cmethod;
@@ -2507,7 +2510,10 @@ reform_and_rewrite_tuple(HeapTuple tuple,
 
 			/* if compression method doesn't match then detoast the value */
 			if (TupleDescAttr(newTupDesc, i)->attcompression != cmethod)
+			{
 				values[i] = PointerGetDatum(detoast_attr(new_value));
+				values_free[i] = true;
+			}
 		}
 	}
 
@@ -2515,6 +2521,13 @@ reform_and_rewrite_tuple(HeapTuple tuple,
 
 	/* The heap rewrite module does the rest */
 	rewrite_heap_tuple(rwstate, tuple, copiedTuple);
+
+	/* Free any value detoasted previously */
+	for (i = 0; i < newTupDesc->natts; i++)
+	{
+		if (values_free[i])
+			pfree(DatumGetPointer(values[i]));
+	}
 
 	heap_freetuple(copiedTuple);
 }