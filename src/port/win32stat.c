sanchitachaurasia/DBIS-Project

commit 10b5cd5b565e75e0a0f81c7f8df97827abba450e

Fix fstat() emulation on Windows with standard streams
The emulation of fstat() in win32stat.c caused two issues with the
existing in-core callers, failing on EINVAL when using a stream as
argument:
- psql's \copy would crash when using a stream.
- pg_recvlogical would fail with -f -.

The tests in copyselect.sql from the main test suite covers the first
case, and there is a TAP test for the second case.  However, in both
cases, as the standard streams are always redirected, automated tests
did not notice those issues, requiring a terminal on Windows to be
reproducible.

This issue has been introduced in 7f85496, and the origin of the problem
is that GetFileInformationByHandle() does not work directly on streams,
so this commit adds an extra code path to emulate and return a set of
stats that match best with the reality.  Note that redirected streams
rely on handles that can be queried with GetFileInformationByHandle(),
but we can rely on GetFinalPathNameByHandleA() to detect this case.

Author: Dmitry Koval, Juan José Santamaría Flecha
Discussion: https://postgr.es/m/17288-6b58a91025a8a8a3@postgresql.org
Backpatch-through: 14

src/port/win32stat.c
@@ -289,11 +289,31 @@ int
 _pgfstat64(int fileno, struct stat *buf)
 {
 	HANDLE		hFile = (HANDLE) _get_osfhandle(fileno);
+	char		path[MAX_PATH];
 
 	if (hFile == INVALID_HANDLE_VALUE || buf == NULL)
 	{
 		errno = EINVAL;
 		return -1;
+	}
+
+	/*
+	 * Check if the fileno is a data stream.  If so, unless it has been
+	 * redirected to a file, getting information through its HANDLE will fail,
+	 * so emulate its stat information in the most appropriate way and return
+	 * it instead.
+	 */
+	if ((fileno == _fileno(stdin) ||
+		 fileno == _fileno(stdout) ||
+		 fileno == _fileno(stderr)) &&
+		GetFinalPathNameByHandleA(hFile, path, MAX_PATH, VOLUME_NAME_NT) == 0)
+	{
+		memset(buf, 0, sizeof(*buf));
+		buf->st_mode = _S_IFCHR;
+		buf->st_dev = fileno;
+		buf->st_rdev = fileno;
+		buf->st_nlink = 1;
+		return 0;
 	}
 
 	/*

commit 5ea8c1e78a7ca35cfa3e3e0e26ec46739116a739

Fix compatibility thinko for fstat() on standard streams in win32stat.c
GetFinalPathNameByHandleA() cannot be used in compilation environments
where _WIN32_WINNT < 0x0600, meaning at least Windows XP used by some
buildfarm members under MinGW that Postgres still needs to support.
This was reported as a compilation warning by the buildfarm, but this is
actually worse than the report as the code would have not worked.

Instead, this switches to GetFileInformationByHandle() that is able to
fail for standard streams and succeed for redirected ones, which is what
we are looking for herein the code emulating fstat().  We also know that
it is able to work in all the environments still supported, thanks to
the existing logic of win32stat.c.

Issue introduced by 10b5cd5, so backpatch down to 14.

Reported-by: Justin Pryzby, via buildfarm member jacana
Author: Michael Paquier
Reviewed-by: Juan José Santamaría Flecha
Discussion: https://postgr.es/m/20211129050122.GK17618@telsasoft.com
Backpatch-through: 14

src/port/win32stat.c
@@ -289,7 +289,7 @@ int
 _pgfstat64(int fileno, struct stat *buf)
 {
 	HANDLE		hFile = (HANDLE) _get_osfhandle(fileno);
-	char		path[MAX_PATH];
+	BY_HANDLE_FILE_INFORMATION fiData;
 
 	if (hFile == INVALID_HANDLE_VALUE || buf == NULL)
 	{
@@ -306,7 +306,7 @@ _pgfstat64(int fileno, struct stat *buf)
 	if ((fileno == _fileno(stdin) ||
 		 fileno == _fileno(stdout) ||
 		 fileno == _fileno(stderr)) &&
-		GetFinalPathNameByHandleA(hFile, path, MAX_PATH, VOLUME_NAME_NT) == 0)
+		!GetFileInformationByHandle(hFile, &fiData))
 	{
 		memset(buf, 0, sizeof(*buf));
 		buf->st_mode = _S_IFCHR;