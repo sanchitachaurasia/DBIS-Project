sanchitachaurasia/DBIS-Project

commit 14a171f7da72f771c45347695b4475a5bb37a1f5

Add missing unicode_category.c to MSVC build scripts
Fixes MSVC build failure introduced by 9a29d5377

src/tools/msvc/Mkvcbuild.pm
@@ -156,6 +156,7 @@ sub mkvcbuild
 		push(@pgcommonallfiles, 'md5.c');
 		push(@pgcommonallfiles, 'sha1.c');
 		push(@pgcommonallfiles, 'sha2.c');
+		push(@pgcommonallfiles, 'unicode_category.c');
 	}
 
 	our @pgcommonfrontendfiles = (

commit ccf7bd9b779c30a100c16c26ce00634fd6db4a09

Try again to fix the MSVC build
My last attempt in 14a171f7d mistakenly conditionally added the missing
file based on some unrelated condition.

Reported-by: Thomas Munro
Discussion: https://postgr.es/m/CA+hUKGLovvAXim9Fytn=jxks9s=JhP5=8Oyy0cbxGG-ggALJtg@mail.gmail.com

src/tools/msvc/Mkvcbuild.pm
@@ -140,8 +140,8 @@ sub mkvcbuild
 	  f2s.c file_perm.c file_utils.c hashfn.c ip.c jsonapi.c
 	  keywords.c kwlookup.c link-canary.c md5_common.c percentrepl.c
 	  pg_get_line.c pg_lzcompress.c pg_prng.c pgfnames.c psprintf.c relpath.c
-	  rmtree.c saslprep.c scram-common.c string.c stringinfo.c unicode_norm.c
-	  username.c wait_error.c wchar.c);
+	  rmtree.c saslprep.c scram-common.c string.c stringinfo.c
+	  unicode_category.c unicode_norm.c username.c wait_error.c wchar.c);
 
 	if ($solution->{options}->{openssl})
 	{
@@ -156,7 +156,6 @@ sub mkvcbuild
 		push(@pgcommonallfiles, 'md5.c');
 		push(@pgcommonallfiles, 'sha1.c');
 		push(@pgcommonallfiles, 'sha2.c');
-		push(@pgcommonallfiles, 'unicode_category.c');
 	}
 
 	our @pgcommonfrontendfiles = (