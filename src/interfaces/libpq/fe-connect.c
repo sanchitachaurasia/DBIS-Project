sanchitachaurasia/DBIS-Project

commit 3543625f0a70ab18bc57ba953ce11d0013f9f3c2

libpq: Discard leading and trailing spaces for parameters and values in URIs
Integer values applied a parsing rule through pqParseIntParam() that
made URIs like this one working, even if these include spaces around
values:
"postgresql://localhost:5432/postgres?keepalives=1 &keepalives_idle=1 "

This commit changes the parsing so as spaces before and after parameters
and values are discarded, offering more consistency with the parsing
that already applied to libpq for integer values in URIs.

Note that %20 can be used in a URI for a space character.  ECPGconnect()
has been discarded leading and trailing spaces around parameters and
values that for a long time, as well.  Like c797009f40ae, this is done
as a HEAD-only change.

Reviewed-by: Yuto Sasaki
Discussion: https://postgr.es/m/Zv3oWOfcrHTph7JK@paquier.xyz

src/interfaces/libpq/fe-connect.c
@@ -6763,9 +6763,9 @@ conninfo_uri_parse_params(char *params,
 static char *
 conninfo_uri_decode(const char *str, PQExpBuffer errorMessage)
 {
-	char	   *buf;
-	char	   *p;
-	const char *q = str;
+	char	   *buf;			/* result */
+	char	   *p;				/* output location */
+	const char *q = str;		/* input location */
 
 	buf = malloc(strlen(str) + 1);
 	if (buf == NULL)
@@ -6775,13 +6775,23 @@ conninfo_uri_decode(const char *str, PQExpBuffer errorMessage)
 	}
 	p = buf;
 
+	/* skip leading whitespaces */
+	for (const char *s = q; *s == ' '; s++)
+	{
+		q++;
+		continue;
+	}
+
 	for (;;)
 	{
 		if (*q != '%')
 		{
-			/* copy and check for NUL terminator */
-			if (!(*(p++) = *(q++)))
-				break;
+			/* if found a whitespace or NUL, the string ends */
+			if (*q == ' ' || *q == '\0')
+				goto end;
+
+			/* copy character */
+			*(p++) = *(q++);
 		}
 		else
 		{
@@ -6816,6 +6826,26 @@ conninfo_uri_decode(const char *str, PQExpBuffer errorMessage)
 			*(p++) = c;
 		}
 	}
+
+end:
+
+	/* skip trailing whitespaces */
+	for (const char *s = q; *s == ' '; s++)
+	{
+		q++;
+		continue;
+	}
+
+	/* Not at the end of the string yet?  Fail. */
+	if (*q != '\0')
+	{
+		libpq_append_error(errorMessage, "trailing data found: \"%s\"", str);
+		free(buf);
+		return NULL;
+	}
+
+	/* Copy NUL terminator */
+	*p = '\0';
 
 	return buf;
 }

src/interfaces/libpq/t/001_uri.pl
@@ -86,6 +86,24 @@ my @tests = (
 		q{user='uri-user' host='host' (inet)},
 		q{},
 	],
+	[
+		# Leading and trailing spaces, works.
+		q{postgresql://host?  user = uri-user & port  = 12345 },
+		q{user='uri-user' host='host' port='12345' (inet)},
+		q{},
+	],
+	[
+		# Trailing data in parameter.
+		q{postgresql://host?  user user  =  uri  & port = 12345 12 },
+		q{},
+		q{libpq_uri_regress: trailing data found: "  user user  "},
+	],
+	[
+		# Trailing data in value.
+		q{postgresql://host?  user  =  uri-user  & port = 12345 12 },
+		q{},
+		q{libpq_uri_regress: trailing data found: " 12345 12 "},
+	],
 	[ q{postgresql://host?}, q{host='host' (inet)}, q{}, ],
 	[
 		q{postgresql://[::1]:12345/db},

commit 445011a0d9c72d939c89b47b931a4e4205bcbfb0

libpq: Improve error message when parsing URI parameters and keywords
The error message showing up when parameters or keywords include too
many whitespaces was "trailing data found", which was confusing because
there was no hint about what was actually wrong.

Issue introduced in 3543625f0a70, hence there is no need for a
backpatch.

Author: Yushi Ogiwara
Reviewed-by: Fujii Masao, Tom Lane, Bruce Momjian
Discussion: https://postgr.es/m/645bd22a53c4da8a1bc7e1e52d9d3b52@oss.nttdata.com

src/interfaces/libpq/fe-connect.c
@@ -6825,7 +6825,9 @@ end:
 	/* Not at the end of the string yet?  Fail. */
 	if (*q != '\0')
 	{
-		libpq_append_error(errorMessage, "trailing data found: \"%s\"", str);
+		libpq_append_error(errorMessage,
+						   "unexpected spaces found in \"%s\", use percent-encoded spaces (%%20) instead",
+						   str);
 		free(buf);
 		return NULL;
 	}

src/interfaces/libpq/t/001_uri.pl
@@ -96,13 +96,13 @@ my @tests = (
 		# Trailing data in parameter.
 		q{postgresql://host?  user user  =  uri  & port = 12345 12 },
 		q{},
-		q{libpq_uri_regress: trailing data found: "  user user  "},
+		q{libpq_uri_regress: unexpected spaces found in "  user user  ", use percent-encoded spaces (%20) instead},
 	],
 	[
 		# Trailing data in value.
 		q{postgresql://host?  user  =  uri-user  & port = 12345 12 },
 		q{},
-		q{libpq_uri_regress: trailing data found: " 12345 12 "},
+		q{libpq_uri_regress: unexpected spaces found in " 12345 12 ", use percent-encoded spaces (%20) instead},
 	],
 	[ q{postgresql://host?}, q{host='host' (inet)}, q{}, ],
 	[