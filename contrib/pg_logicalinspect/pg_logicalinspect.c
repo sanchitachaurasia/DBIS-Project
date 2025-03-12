sanchitachaurasia/DBIS-Project

commit cf05123f08a5ccdd0392c392dd2a691a069abf51

Fix compiler warning in pg_logicalinspect.
Oversight in c9916fc2c6b.

Reported-by: David Rowley <dgrowleyml@gmail.com>
Reported-by: Nathan Bossart <nathandbossart@gmail.com>
Author: David Rowley <dgrowleyml@gmail.com>
Discussion: https://postgr.es/m/CAApHDvqrhFfnetbcwgGkJ=z63T8HfQ_OyP=vX8BYiXyxFKt67w@mail.gmail.com

contrib/pg_logicalinspect/pg_logicalinspect.c
@@ -85,6 +85,8 @@ parse_snapshot_filename(const char *filename)
 parse_error:
 	ereport(ERROR,
 			errmsg("invalid snapshot file name \"%s\"", filename));
+
+	return InvalidXLogRecPtr;					/* keep compiler quiet */
 }
 
 /*

commit 77a359cab85fe45ce34e0e9f31b4fbd0da6e0b23

Fix indentation issue
Introduced recently by cf05123f0

Per buildfarm member koel

contrib/pg_logicalinspect/pg_logicalinspect.c
@@ -86,7 +86,7 @@ parse_error:
 	ereport(ERROR,
 			errmsg("invalid snapshot file name \"%s\"", filename));
 
-	return InvalidXLogRecPtr;					/* keep compiler quiet */
+	return InvalidXLogRecPtr;	/* keep compiler quiet */
 }
 
 /*